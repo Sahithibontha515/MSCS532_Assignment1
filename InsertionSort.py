dcfa7fdf649d3f4d01aa41c87e99613084ad0cf3
Sahithibontha515 <sahithibontha515@gmail.com>
2025-06-30 22:04:46 +0530

    added sample values to test insertion sort

--- InsertionSort.py
+++ InsertionSort.py
@@ -9,3 +9,7 @@ def apply_descending_insertion_sort(elements):
             elements[j+1] = elements[j]
             j = j-1
         elements[j+1] = key
+# replace the elements with different values 
+elements = [11,0,9,100,1,4,5,19]
+apply_descending_insertion_sort(elements)
+print(elements)
